667198c24564ec98970b167752c291f7037c77e4
hand-dot <kyouhei.fukuda0729@gmail.com>
2018-10-12 23:47:51 +0900

    add: Error Test case

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const units = ['kg', 'Nm3', 'Sm3', 'L'];
 const gases = ['O2', 'N2', 'Ar', 'H2', 'CO2', 'He', 'C2H2'];
-
-module.exports = function (gas, amount, inputUnit, outputUnit) {
-    if (gas === undefined || amount === undefined || inputUnit === undefined || outputUnit === undefined) {
+/**
+ * 関数
+ * @param {String} gas ['O2', 'N2', 'Ar', 'H2', 'CO2', 'He', 'C2H2']
+ * @param {String} inputUnit ['kg', 'Nm3', 'Sm3', 'L']
+ * @param {String} outputUnit ['kg', 'Nm3', 'Sm3', 'L']
+ * @param {Number} input input amount
+ * @return {Number} output amount
+ * @throws {String} invalid arguments
+ */
+module.exports = function (gas, inputUnit, outputUnit, input) {
+    if (gas === undefined || inputUnit === undefined || outputUnit === undefined || input === undefined) {
         throw new Error("Error: invalid arguments");
     }
-    if (!gases.includes(gas) && !isNaN(amount) && !units.includes(inputUnit) && !units.includes(outputUnit)) {
+    if (!gases.includes(gas) || !units.includes(inputUnit) || !units.includes(outputUnit) || isNaN(input)) {
         throw new Error("Error: invalid arguments");
     }
+    console.log('!!')
     return 'hello'
 }
--- test/test.js
+++ test/test.js
@@ -1,7 +1,24 @@
 var assert = require('assert');
 var guc = require('../index.js');
-describe('Test of guc', function () {
-    it('invalid arguments', function () {
+describe('Error Test of guc', function () {
+    it('Error: invalid arguments by nothing arguments', () => {
         assert.throws(() => guc(), Error, "Error: invalid arguments");
     });
+    it('Error: invalid arguments by invalid gas', () => {
+        assert.throws(() => guc('hey', 'kg', 'L', 1), Error, "Error: invalid arguments");
+    });
+    it('Error: invalid arguments by invalid inputUnit', () => {
+        assert.throws(() => guc('O2', 'hoge', 'L', 1), Error, "Error: invalid arguments");
+    });
+    it('Error: invalid arguments by invalid outputUnit', () => {
+        assert.throws(() => guc('O2', 'kg', 'foo', 1), Error, "Error: invalid arguments");
+    });
+    it('Error: invalid arguments by invalid inputUnit and invalid outputUnit', () => {
+        assert.throws(() => guc('O2', 'hoge', 'foo', 1), Error, "Error: invalid arguments");
+    });
+    it('Error: invalid arguments by invalid input', () => {
+        assert.throws(() => guc('O2', 'kg', 'L', '!'), Error, "Error: invalid arguments");
+    });
+});
+describe('Nominal Test of guc', function () {
 });
